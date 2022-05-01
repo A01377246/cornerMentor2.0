1d5a1650e0c4e1cd8c47ad4e7f7ac3e4c9e551d6
Rafael Romero <A01747730@itesm.mx>
2022-04-30 21:27:34 -0500

    1.-Array Materia

--- app/src/main/java/mx/itesm/cornermentor20/MainActivity.kt
+++ app/src/main/java/mx/itesm/cornermentor20/MainActivity.kt
@@ -1,23 +1,31 @@
 package mx.itesm.cornermentor20
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import mx.itesm.cornermentor20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var arrayList: ArrayList<Materia>
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var adaptadorMateria: AdaptadorMaterias? = null
 
         val navView: BottomNavigationView = binding.navView
 
@@ -31,5 +39,12 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //ArrayMaterias
+        val arrayMateria= arrayOf(Materia("Matematicas"))
+        val layout=LinearLayoutManager(this)
+        layout.orientation=LinearLayoutManager.VERTICAL
+        adaptadorMateria=AdaptadorMaterias(this,arrayMateria)
+        binding.rvLista1.adapter=adaptadorMateria
     }
 }
