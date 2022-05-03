5ce16849e2dbab3e636723ed515b55397ced2e3c
Rafael Romero <A01747730@itesm.mx>
2022-05-03 01:25:42 -0500

    mas cambios

---
+++ app/src/main/java/mx/itesm/cornermentor20/pantallacreditos.kt
@@ -0,0 +1,11 @@
+package mx.itesm.cornermentor20
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class pantallacreditos: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pantallacreditos)
+    }
+}
